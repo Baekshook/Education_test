6aee3e98562b6c800da1e5ae7945cfe5d5d826b0
Baekshook <ecgo0106@naver.com>
2026-04-30 11:23:49 +0900

    ✨ Create First Settings
    Create First Settings

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      g
+    </>
+  );
+}
+
+export default App;
